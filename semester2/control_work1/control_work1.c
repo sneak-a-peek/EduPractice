7d79b2af6a31e0eef2a2beceaac25e22ffb446f8
stepan <stepan@T34.home>
2024-06-29 11:27:57 +0300

    10: Added some time ago changed stuff

--- semester2/control_work1/control_work1.c
+++ semester2/control_work1/control_work1.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int **Y, int n, int m){
-    int max, i, j, k = 0, d;
-    max = Y[0][0];
-    for (i = 0; i < m; i++)
-    for (j = 0; j < n; j++)
-    if (Y[j][i]>max) {max = Y[j][i]; k=i;}
-    d=0;
-    printf("%d\n", k);
-    for(i=0;i<n;i++) d+=Y[i][k];
-    return d; }
+int main(int argc, char *argv[]) {
+    FILE *f = fopen("data.txt", "r");
+    FILE *f1 = fopen("data.txt", "r+");
+    int i = 0,l;
+    int w;
+    while (!feof(f)) 
+    {
+        l = ftell(f);
+        if (fscanf(f, "%d", &w) == 1)
+        if (w%2 == 0) {fseek(f1, l, SEEK_SET);
+        fprintf(f1, "%s", "\40\40");}
+    }
+    fclose(f);
+    fclose(f1);
+    system("PAUSE");
+    return 0;
+}
